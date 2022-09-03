cbab10555c3ce925a7f0ace7fedc8b367029dd45
George <urban2014m@gmail.com>
2022-09-03 17:22:59 +0300

    Находим максимальное число через функции

---
+++ Example010_IntroMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿//Находим максимум из трёх через функции
+int Max (int arg1, int arg2, int arg3)
+{
+    int resalt = arg1;
+    if(arg2 > resalt) resalt = arg2;
+    if(arg3 > resalt) resalt = arg3;
+    return resalt;
+}
+
+
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 3959;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 3130;
+int b3 = 23;
+int c3 = 31;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
